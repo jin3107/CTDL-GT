ab1de980b6ea8cc868c64c0c9a3527a66b4f61a2
Chuong Huynh <tanchuonghuynh3@gmail.com>
2025-02-13 12:13:28 +0700

    Student management

---
+++ StudentManagement/StudentManagement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace StudentManagement
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập số lượng sinh viên: ");
+            int N = int.Parse(Console.ReadLine());
+            StudentManager manager = new StudentManager(N);
+
+            Console.WriteLine("\nChọn cách nhập điểm:");
+            Console.WriteLine("1. Nhập từ bàn phím");
+            Console.WriteLine("2. Khởi tạo sẵn");
+            Console.WriteLine("3. Đọc từ file");
+            Console.WriteLine("4. Phát sinh ngẫu nhiên");
+            Console.Write("Lựa chọn: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: manager.NhapTuBanPhim(); break;
+                case 2: manager.NhapTuPhatSinh(); break;
+                case 3: manager.NhapTuFile(); break;
+                case 4: manager.DauVaoNgauNhien(); break;
+                default: Console.WriteLine("Lựa chọn không hợp lệ!"); return;
+            }
+
+            manager.DoLuongHieuSuat();
+        }
+    }
+}
---
+++ StudentManagement/StudentManagement/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public int M1 { get; set; }
+        public int M2 { get; set; }
+        public int M3 { get; set; }
+        public double Average => (M1 + M2 + M3) / 3.0;
+    }
+}
