0ccb3702bdb730c251072fb90dfd1335c5e282d3
Chuong Huynh <tanchuonghuynh3@gmail.com>
2025-02-13 12:52:24 +0700

    Swap generic

---
+++ SwapGeneric/SwapGeneric/Entities/NhanVien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapGeneric
+{
+    public struct NhanVien
+    {
+        public string HoTen;
+        public int MaNhanVien;
+        public double HeSoLuong;
+
+        public NhanVien(string hoTen, int maNhanVien, double heSoLuong)
+        {
+            HoTen = hoTen;
+            MaNhanVien = maNhanVien;
+            HeSoLuong = heSoLuong;
+        }
+
+        public override string ToString()
+        {
+            return $"{MaNhanVien} - {HoTen} - HSL: {HeSoLuong}";
+        }
+    }
+}
---
+++ SwapGeneric/SwapGeneric/Handlers/InputHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SwapGeneric
+{
+    public static class InputHandler
+    {
+        public static int ReadInt(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static double ReadDouble(string message)
+        {
+            Console.Write(message);
+            return double.Parse(Console.ReadLine());
+        }
+
+        public static string ReadString(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+    }
+}
